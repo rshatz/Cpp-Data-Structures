1b8129d0e39a55c4f5ffdfdf25de9d51a801684a
rshatz <richshatz@gmail.com>
2016-05-07 18:46:36 -0700

    moved map->BlackJack.h & set->Deck.h

--- Project/BlackJack/Deck.cpp
+++ Project/BlackJack/Deck.cpp
@@ -3,20 +3,21 @@
 #include <vector>
 #include <algorithm>
 
+using std::vector;
+
 void Deck::shuffle()
 {
-    using std::vector;
     vector<string> vect;
 
-    map<string, int>::const_iterator imap;
-    for (imap = cards.begin(); imap != cards.end(); imap++) {
-       vect.push_back(imap->first);
+    set<string>::const_iterator iset;
+    for(iset = cards.begin(); iset != cards.end(); iset++) {
+        vect.push_back(*iset);
     }
 
     random_shuffle(vect.begin(), vect.end());
 
     vector<string>::const_iterator ivect = vect.begin();
-    while (ivect != vect.end()) {
+    while(ivect != vect.end()) {
         deck.push(*ivect);
         ivect++;
     }
